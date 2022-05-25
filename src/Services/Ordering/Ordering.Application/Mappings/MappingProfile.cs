054d5e4e6fcf597a9e9022f8b32b420375347b9e
EdivanCOliveira <96794227+EdivanCOliveira@users.noreply.github.com>
2022-05-25 16:56:48 -0300

    Construindo a Ordering.API

--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
 using Ordering.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -12,8 +14,8 @@ namespace Ordering.Application.Mappings
     {
         public MappingProfile()
         {
-            //CreateMap<Order, OrdersVm>().ReverseMap();
-            //CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<Order, OrdersVm>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
             //CreateMap<Order, UpdateOrderCommand>().ReverseMap();
         }
     }
